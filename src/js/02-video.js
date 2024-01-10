14eae2aed37230461f823308e9dd6d3efc00b44d
Eugene asaulyak <eugene.asaulyak@gmail.com>
2024-01-10 11:34:57 +0200

    added video playback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const PLAYER_CURRENT_TIME_KEY = 'videoplayer-current-time'
+
+function createPlayer() {
+  const iframe = document.getElementById('vimeo-player');
+  return new Player(iframe);
+}
+
+function bindVimeoEvents(player) {
+  player.on('timeupdate', throttle(event => setVideoProgress(event.seconds), 1000));
+  player.on('ended', () => setVideoProgress(0));
+
+  player.on('loaded', () => {
+    const currentProgress = getVideoProgress();
+
+    if (!currentProgress) {
+      return;
+    }
+
+    player.setCurrentTime(currentProgress);
+  });
+}
+
+function setVideoProgress(position) {
+  localStorage.setItem(PLAYER_CURRENT_TIME_KEY, position);
+}
+
+function getVideoProgress() {
+  return localStorage.getItem(PLAYER_CURRENT_TIME_KEY);
+}
+
+function init() {
+  const player = createPlayer();
+  bindVimeoEvents(player);
+}
+
+init();
