e7e93c3ee6706b952d88c45600559d62db1f8bd1
Eugene asaulyak <eugene.asaulyak@gmail.com>
2024-01-10 09:34:37 +0200

    added gallery

---
+++ src/js/01-gallery.js
@@ -0,0 +1,45 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRoot = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+             <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+           </a>
+          </li>`;
+}
+
+function createGallery() {
+  const galleryHtmlElements = galleryItems.map(item => createGalleryItem(item)).join('');
+
+  if (!galleryRoot) {
+    return;
+  }
+
+  galleryRoot.insertAdjacentHTML('beforeend', galleryHtmlElements);
+}
+
+function bindEvents() {
+  galleryRoot.querySelectorAll('.gallery__link').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+    });
+  });
+}
+
+function createLightBox() {
+  return new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt'
+  });
+}
+
+createGallery();
+bindEvents();
+createLightBox();
